db738b8331e309fd9f658ffec84b4d9c6da97411
A1AN2 <alan.agudelo.sa@gmail.com>
2026-05-30 09:02:03 -0500

    wp4

--- Workshop_4_Simulation/person_1_user_flow_simulation/code/registration.py
+++
@@ -1,9 +0,0 @@
-def run_registration(num_students, registration_failure_rate=0.0):
-    registered = []
-    num_failed = 0
-    for i in range(num_students):
-        if random.random() > registration_failure_rate:
-            registered.append(i)
-        else:
-            num_failed += 1
-    return registered, len(registered), num_failed
